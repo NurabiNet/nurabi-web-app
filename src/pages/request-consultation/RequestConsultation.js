72f59ec112d51d9270d63f5f6d3ddf058ff1cdaa
ahmedhamam010 <ahmedhamam454546@gmail.com>
2022-12-26 23:02:54 +0200

    remove unncessary imoorts in request consultation page

--- src/pages/request-consultation/RequestConsultation.js
+++ src/pages/request-consultation/RequestConsultation.js
@@ -1,7 +1,5 @@
 import { Col, Container, Row } from "react-bootstrap"
 import Form from 'react-bootstrap/Form';
-import { FaArrowLeft } from "react-icons/fa"
-import { NavLink } from "react-router-dom"
 import MainButton from "../../UI/main-button/MainButton"
 import classes from './RequestConsultation.module.css'
 
